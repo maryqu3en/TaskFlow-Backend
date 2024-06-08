a9c504d132138c649d92cac4aa4e1e7ee40041a6
meriem soubih <soubih.meriem@gmail.com>
2024-06-08 15:05:32 +0100

    little update

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
-const taskRouter = require('./routes/task.route');
+const taskRouter = require('./routes/task.routes');
 const authRouter = require('./routes/auth.routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 const connectDB = () => mongoose.connect(process.env.MONGODB_URI);
 
 app.get('/', (req, res) => {
-  res.send("Hello World!");
+  res.send("Welcome to TaskFlow, a task manager API.");
 });
 
 app.use(taskRouter);
