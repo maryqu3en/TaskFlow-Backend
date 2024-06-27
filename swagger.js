67f5689a2a7f14abfc50201c1c6dadc879448e31
Meriem Soubih <soubih.meriem@gmail.com>
2024-06-27 11:04:34 +0100

    swagger docs config

---
+++ swagger.js
@@ -0,0 +1,24 @@
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "TaskFlow API",
+      version: "1.0.0",
+      description: "API documentation for the TaskFlow backend",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "Local server",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+module.exports = { swaggerUi, specs };
