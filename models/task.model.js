c7bd4ea5a6b9cbe30053a055a1c3051564181132
Meriem Soubih <soubih.meriem@gmail.com>
2024-06-27 19:31:41 +0100

    updated models and added token collection

--- models/task.model.js
+++ models/task.model.js
@@ -1,31 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: false
-    },
-    status: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User"
-    }
-}, {
-    timestamps: true
+    title: { type: String, required: true },
+    description: { type: String },
+    status: { type: String, enum: ['Pending', 'In Progress', 'Completed'], default: 'Pending' },
+    date: { type: Date, default: Date.now },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    deletedAt: { type: Date },
+    isDeleted: { type: Boolean, default: false },
+    owner: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
 const Task = mongoose.model("Task", TaskSchema);
-
 module.exports = Task;
--- models/user.model.js
+++ models/user.model.js
@@ -1,65 +1,28 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    tokens: [{
-        token: {
-            type: String,
-            required: true
-        }
-    }]
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    deletedAt: { type: Date },
+    isDeleted: { type: Boolean, default: false }
 });
 
-UserSchema.pre('save', async function(next){
+UserSchema.pre('save', async function (next) {
     const user = this;
-    if(user.isModified('password')){
+    if (user.isModified('password')) {
+
         user.password = await bcrypt.hash(user.password, 8);
     }
     next();
 });
 
-UserSchema.methods.generateAuthToken = async function(){
-    const user = this;
-    const token = jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET);
-    user.tokens = user.tokens.concat({token});
-    await user.save();
-    return token;
-}
-
-UserSchema.methods.removeToken = async function(token) {
-    const user = this;
-    user.tokens = user.tokens.filter((tokenDoc) => tokenDoc.token !== token);
-    await user.save();
-}
-
-UserSchema.statics.findByCredentials = async (email, password) => {
-    const user = await User.findOne({email});
-    if(!user){
-        throw new Error('Unable to login: user does not exist');
-    }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if(!isMatch){
-        throw new Error('Unable to login: password doesnt match');
-    }
-    return user;
-}
+userSchema.methods.comparePassword = function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
