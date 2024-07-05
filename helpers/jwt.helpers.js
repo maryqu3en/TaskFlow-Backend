45365da830a4cd378e4c1a6677b61f9315d44f8e
Meriem Soubih <soubih.meriem@gmail.com>
2024-07-05 08:47:31 +0100

    added jwt helper

--- helpers/jwt.helpers.js
+++ helpers/jwt.helpers.js
@@ -1,16 +1,25 @@
 const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
+const Token = require('../models/token.model')
 
-const generateToken = (user) => {
+const generateToken = async (user) => {
   const payload = {
-    id: user._id,
-    email: user.email,
+      id: user._id,
+      email: user.email,
   };
-  return jwt.sign(payload, secret, { expiresIn: '10d' });
+  const token = jwt.sign(payload, secret, { expiresIn: '10d' });
+  const expiresAt = new Date(Date.now() + 10 * 24 * 3600 * 1000 );
+
+  await Token.create({ token, userId: user._id, expiresAt });
+  return token;
 };
 
 const verifyToken = (token) => {
   return jwt.verify(token, secret);
 };
 
-module.exports = { generateToken, verifyToken };
+const deleteToken = async (token) => {
+  await Token.findOneAndDelete({ token });
+};
+
+module.exports = { generateToken, verifyToken, deleteToken };
